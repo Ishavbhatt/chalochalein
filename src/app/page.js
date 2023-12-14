24c935a341e5e4c0dbc3e047f4b4cd7d9647bb88
komal-108 <komalbadyal10@gmail.com>
2023-12-14 17:05:33 +0530

    footer

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './styles/pages/page.module.scss'
+import Header from './components/Header/Header'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    </main>
+  )
+}
