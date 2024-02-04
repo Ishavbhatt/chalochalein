1c6a1ffad0b716844e89028beb263268504072d3
Ishavbhatt <ishavbhattkhaniyara@gmail.com>
2024-02-04 11:39:27 +0530

    add google tag manager

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
