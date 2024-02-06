7eaa1e1085a4612d8fa5f9e22d709058f25d9099
komal-108 <komalbadyal10@gmail.com>
2024-02-06 21:38:49 +0530

    update images

--- app/components/Activities/activities.js
+++ app/components/Activities/activities.js
@@ -17,13 +17,13 @@ const Activities = () => {
     {
       activity_name: 'Paragliding',
       short_description: 'Known for its ancient temples, vibrant culture, and stunning natural beauty.',
-      activity_image: '/images/adventure-activities/paragliding.jpg',
+      activity_image: '/images/adventure-activities/paragliding.webp',
       icon: <FaParachuteBox />
     },
     {
       activity_name: 'Camping',
       short_description: 'A cold desert mountain valley famous for its landscapes, monasteries, and adventure expeditions.',
-      activity_image: '/images/adventure-activities/camping.jpg',
+      activity_image: '/images/adventure-activities/camping.webp',
       icon: <GiCampingTent />
     },
   ];
